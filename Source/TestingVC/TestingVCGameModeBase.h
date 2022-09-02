7eb601ab748645cb860e26ef7e2d8de18ac14722
Connor-FMOD <connor@fmod.com>
2022-09-02 15:07:01 +1000

    Initial commit

---
+++ Source/TestingVC/TestingVCGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestingVCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGVC_API ATestingVCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
