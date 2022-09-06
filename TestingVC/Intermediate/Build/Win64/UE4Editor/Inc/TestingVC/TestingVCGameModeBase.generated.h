7506f20cf745fb172d234cbf59b2938b0f0ab12f
Connor Young <connor@fmod.com>
2022-09-06 10:13:51 +1000

    Initial Commit attempt 2

---
+++ TestingVC/Intermediate/Build/Win64/UE4Editor/Inc/TestingVC/TestingVCGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TESTINGVC_TestingVCGameModeBase_generated_h
+#error "TestingVCGameModeBase.generated.h already included, missing '#pragma once' in TestingVCGameModeBase.h"
+#endif
+#define TESTINGVC_TestingVCGameModeBase_generated_h
+
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_SPARSE_DATA
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_RPC_WRAPPERS
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATestingVCGameModeBase(); \
+	friend struct Z_Construct_UClass_ATestingVCGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATestingVCGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TestingVC"), NO_API) \
+	DECLARE_SERIALIZER(ATestingVCGameModeBase)
+
+
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATestingVCGameModeBase(); \
+	friend struct Z_Construct_UClass_ATestingVCGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATestingVCGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TestingVC"), NO_API) \
+	DECLARE_SERIALIZER(ATestingVCGameModeBase)
+
+
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATestingVCGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATestingVCGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATestingVCGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATestingVCGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATestingVCGameModeBase(ATestingVCGameModeBase&&); \
+	NO_API ATestingVCGameModeBase(const ATestingVCGameModeBase&); \
+public:
+
+
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATestingVCGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATestingVCGameModeBase(ATestingVCGameModeBase&&); \
+	NO_API ATestingVCGameModeBase(const ATestingVCGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATestingVCGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATestingVCGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATestingVCGameModeBase)
+
+
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_12_PROLOG
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_SPARSE_DATA \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_RPC_WRAPPERS \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_INCLASS \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_SPARSE_DATA \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	TestingVC_Source_TestingVC_TestingVCGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TESTINGVC_API UClass* StaticClass<class ATestingVCGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TestingVC_Source_TestingVC_TestingVCGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
