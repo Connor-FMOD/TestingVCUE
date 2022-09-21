f8dd121024c4b77afa6ad82396a3d7618580b4cc
Connor Young <connor@fmod.com>
2022-09-21 11:19:43 +1000

    Restating Repo

--- TestingVC/Intermediate/Build/Win64/TestingVCEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30146
-#include "E:/UE Projects/TestingVC/TestingVC/Intermediate/Build/Win64/TestingVCEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- TestingVC/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30146
-#include "E:/UE Projects/TestingVC/TestingVC/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.h"
